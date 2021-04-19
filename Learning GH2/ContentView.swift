4e0fa9590c2f0518a4d6a134e72c87e28c97ee1b
Jola Turzynska <jolanta.turzynska@gmail.com>
2021-04-20 00:04:10 +0200

    Initial Commit

---
+++ Learning GH2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Learning GH2
+//
+//  Created by Jolanta Turzynska on 19/04/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Learning GH2/Learning_GH2App.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_GH2App.swift
+//  Learning GH2
+//
+//  Created by Jolanta Turzynska on 19/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct Learning_GH2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
